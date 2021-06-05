19821b238dec94a0ebd35f7b5f6b84c502e4e157
jco872@gmail.com <jco872@gmail.com>
2021-06-05 11:17:23 -0400

    more updates

--- WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
+++ WiredBrainCoffee.CustomersApp/MainPage.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Linq;
+using Windows.ApplicationModel;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using WiredBrainCoffee.CustomersApp.DataProvider;
+using WiredBrainCoffee.CustomersApp.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -9,12 +13,39 @@ namespace WiredBrainCoffee.CustomersApp
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    /// </summary> 
     public sealed partial class MainPage : Page
     {
+        public CustomerDataProvider _customerDataProvider { get; }
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            App.Current.Suspending += App_Suspending;
+            _customerDataProvider = new CustomerDataProvider();
+        }
+
+
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            customerListView.Items.Clear();
+            var customers = await _customerDataProvider.LoadCustomersAsync();
+
+            foreach (var customer in customers)
+            {
+                customerListView.Items.Add(customer);
+            }
+        }
+
+        private async void App_Suspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await _customerDataProvider.SaveCustomersAsync(
+                customerListView.Items.OfType<Customer>());
+
+            deferral.Complete();
         }
 
         private async void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
@@ -31,8 +62,10 @@ namespace WiredBrainCoffee.CustomersApp
         private void ButtonMove_Click(object sender, RoutedEventArgs e)
         {
             int column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            int newColumns = column == 0 ? 2 : 0;
 
-            customerListGrid.SetValue(Grid.ColumnProperty, 2);
+            moveSymbolIcon.Symbol = newColumns == 0 ? Symbol.Forward : Symbol.Back;
+            customerListGrid.SetValue(Grid.ColumnProperty, newColumns);
         }
     }
 }
